004e49a1363392f824a44bec77d3411dbbd8d9a1
TonyVSK <155448119+TonyVSK@users.noreply.github.com>
2025-11-01 21:47:41 -0300

    pre-processamento dados

--- main.py
+++ main.py
@@ -14,3 +14,21 @@ df = pd.read_csv("WA_Fn-UseC_-Telco-Customer-Churn.csv")
 
 # testar upload do banco:
 print(df.head())
+
+
+
+##################### ETAPA METODOLOGIA 3.1 : TRARAR PRE-PROCESSAMENTO #############################
+
+
+df = df.drop(['customerID'], axis=1) # com isso removemos as colunas irrelevantes, como customerID
+df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors='coerce')
+df = df.dropna() # Com isso normalizamos para numerico para não dar problema
+label_encoders = {}  # Codificar variáveis categóricas
+for column in df.select_dtypes(include=['object']).columns:
+    le = LabelEncoder()
+    df[column] = le.fit_transform(df[column])
+    label_encoders[column] = le
+
+X = df.drop('Churn', axis=1) 
+y = df['Churn'] # em x e y separamos variáveis preditoras e alvo
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y) # dados em 70 porcento e 30 porcento 
